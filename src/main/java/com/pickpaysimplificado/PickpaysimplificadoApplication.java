e1d94b1d9cb70753a499bc64da5277ca9ed3b3b2
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-08-06 09:07:45 -0300

    initial commit

---
+++ src/main/java/com/pickpaysimplificado/PickpaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.pickpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpaysimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickpaysimplificado/PickpaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
